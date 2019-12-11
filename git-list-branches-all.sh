5b8717ff54b770860a9a7ba66c540db55cc44d2c
Michael Dockter <michael@dockter.com>
2019-12-11 16:45:40 -0500

    #1 Original version

---
+++ git-list-branches-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set -x
+REPO_HOME=$(echo ${PWD})
+
+cd $REPO_HOME
+for d in */ ; do
+    echo ""
+    echo "${d}"
+    cd $REPO_HOME/$d
+    branch=$(git branch -r)
+    echo "${branch}"
+done
+
